0524d5abdf37bd80aeb9a9b2498ca190a0d3edc4
Kowies <kowieski.m98@gmail.com>
2019-04-14 19:56:48 +0200

    Initial commit

---
+++ src/main/java/CHIP8.java
@@ -0,0 +1,28 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class CHIP8 extends Application {
+
+    Button button;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.setTitle("CHIP-8 emulator");
+        button = new Button();
+        button.setText("Elo World");
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+}
