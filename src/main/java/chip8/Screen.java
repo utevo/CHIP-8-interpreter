9262ad099bad4d16dc0cff475a2f2dc2b1945e46
Kowies <kowieski.m98@gmail.com>
2019-04-28 14:57:35 +0200

    Update class Screen

--- src/main/java/chip8/Screen.java
+++ src/main/java/chip8/Screen.java
@@ -1,13 +1,28 @@
 package chip8;
 
-import lombok.Data;
-
-@Data
 public class Screen {
 
     final static int HEIGHT = 32;
     final static int WIDTH = 64;
 
     private boolean pixels[][] = new boolean[HEIGHT][WIDTH];
+
+
+    public void setPixel(int x, int y) {
+        pixels[x][y] = true;
+    }
+
+    public void flipPixel(int x, int y) {
+        if (pixels[x][y] == true)
+            pixels[x][y] = false;
+        else
+            pixels[x][y] = true;
+    }
+
+    public boolean getPixel(int x, int y) {
+        return pixels[x][y];
+    }
+
+
 
 }
