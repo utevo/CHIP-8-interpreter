737d9238f370f4748eec787eb08604abf462372a
Kowies <kowieski.m98@gmail.com>
2019-05-10 18:03:26 +0200

    Implement fetching Opcodes and interpreting Opcodes for class CPU

--- src/main/java/chip8/CPU.java
+++ src/main/java/chip8/CPU.java
@@ -10,7 +10,6 @@ public class CPU {
     private Keyboard keyboard;
     private Screen screen;
 
-    @Getter
     boolean changeOnScreen = false;
 
     public CPU(Memory memory, Keyboard keyboard, Screen screen) {
@@ -20,62 +19,406 @@ public class CPU {
     }
 
     private char fetchOpcode() {
-        return  (char)((memory.RAM[memory.PC] << 8) | (memory.RAM[memory.PC + 1]));
+        return (char)((memory.RAM[memory.PC] << 8) | (memory.RAM[memory.PC + 1]));
     }
 
-    public void nextTick() {
+    public void nextTick() throws IllegalStateException {
         char opcode = fetchOpcode();
-
-        switch (opcode) {
-
-            //  Opcode: 00E0
-            //  Explanation: Clears the screen.
-            case 0x00E0:
-
-                return;
-
-            //  Opcode: 00E0
-            //  Explanation: Returns from a subroutine.
-            case 0x00EE:
-
-                return;
-        }
 
         switch (opcode & 0xF000) {
 
-            //  Opcode: 1NNN
-            //  Explanation: Jumps to address NNN.
+            case 0x0000:
+                if ((opcode & 0x00FF) == 0x00E0) {
+                    opcode00E0();
+                    return;
+                }
+                else if ((opcode & 0x00FF) == 0x00EE) {
+                    opcode00EE();
+                    return;
+                }
+                else {
+                    throw new IllegalStateException("WRONG OPCODE");
+                }
+
             case 0x1000:
-
+                opcode1NNN();
                 return;
 
-            //  Opcode: 2NNN
-            //  Explanation: Calls subroutine at NNN.
             case 0x2000:
-
+                opcode2NNN();
                 return;
 
-            //  Opcode: 3XNN
-            //  Explanation: Skips the next instruction if VX equals NN.
-            //  (Usually the next instruction is a jump to skip a code block)
             case 0x3000:
-
+                opcode3XNN();
                 return;
 
-            //  Opcode: 4XNN
-            //  Explanation: Skips the next instruction if VX doesn't equal NN.
-            //  (Usually the next instruction is a jump to skip a code block)
             case 0x4000:
-
+                opcode4XNN();
                 return;
 
-        }
+            case 0x5000:
+                opcode5XY0();
+                return;
 
-        }
+            case 0x6000:
+                opcode6XNN();
+                return;
 
+            case 0x7000:
+                opcode7XNN();
+                return;
+
+            case 0x8000:
+                switch (opcode & 0xF00F) {
+
+                    case 0x8000:
+                        opcode8XY0();
+                        return;
+
+                    case 0x8001:
+                        opcode8XY1();
+                        return;
+
+                    case 0x8002:
+                        opcode8XY2();
+                        return;
+
+                    case 0x8003:
+                        opcode8XY3();
+                        return;
+
+                    case 0x8004:
+                        opcode8XY4();
+                        return;
+
+                    case 0x8005:
+                        opcode8XY5();
+                        return;
+
+                    case 0x8006:
+                        opcode8XY6();
+                        return;
+
+                    case 0x8007:
+                        opcode8XY7();
+                        return;
+
+                    case 0x800E:
+                        opcode8XYE();
+                        return;
+
+                    default:
+                        throw new IllegalStateException("WRONG OPCODE!");
+                }
+
+            case 0x9000:
+                opcode9XY0();
+                return;
+
+            case 0xA000:
+                opcodeANNN();
+                return;
+
+            case 0xB000:
+                opcodeBNNN();
+                return;
+
+            case 0xC000:
+                opcodeCXNN();
+                return;
+
+            case 0xD000:
+                opcodeDXYN();
+                return;
+
+            case 0xE000:
+                if ((opcode & 0xF0FF) == 0xE09E) {
+                    opcodeEX9E();
+                    return;
+                }
+                else if ((opcode & 0xF0FF) == 0xE0A1) {
+                    opcodeEXA1();
+                    return;
+                }
+                else {
+                    throw new IllegalStateException("WRONG OPCODE");
+                }
+
+            case 0xF000:
+                switch (opcode & 0xF0FF) {
+
+                    case 0xF007:
+                        opcodeFX07();
+                        return;
+
+                    case 0xF00A:
+                        opcodeFX0A();
+                        return;
+
+                    case 0xF015:
+                        opcodeFX15();
+                        return;
+
+                    case 0xF018:
+                        opcodeFX18();
+                        return;
+
+                    case 0xF01E:
+                        opcodeFX1E();
+                        return;
+
+                    case 0xF029:
+                        opcodeFX29();
+                        return;
+
+                    case 0xF033:
+                        opcodeFX33();
+                        return;
+
+                    case 0xF055:
+                        opcodeFX55();
+                        return;
+
+                    case 0xF065:
+                        opcodeFX65();
+                        return;
+
+                    default:
+                        throw new IllegalStateException("WRONG OPCODE");
+                }
+
+            default:
+                throw new IllegalStateException("WRONG OPCODE");
+        }
 
     }
 
+    //  Explanation: Clears the screen.
+    public void opcode00E0() {
+        screen.clear();
+        changeOnScreen = true;
+        memory.PC += 2;
+    }
 
+    //  Explanation: Returns from a subroutine.
+    public void opcode00EE() {
+        memory.PC = memory.stack[memory.SP--];
+    }
+
+    //  Explanation: Jumps to address NNN.
+    public void opcode1NNN() {
+        char address = (char) (fetchOpcode() & 0x0FFF);
+        memory.PC = address;
+    }
+
+    //  Explanation: Calls subroutine at NNN.
+    public void opcode2NNN() {
+
+    }
+
+    //  Explanation: Skips the next instruction if VX equals NN.
+    //  (Usually the next instruction is a jump to skip a code block)
+    public void opcode3XNN() {
+
+    }
+
+    //  Explanation: Skips the next instruction if VX doesn't equal NN.
+    //  (Usually the next instruction is a jump to skip a code block)
+    public void opcode4XNN() {
+
+    }
+
+    //  Explanation: Skips the next instruction if VX equals VY.
+    //  (Usually the next instruction is a jump to skip a code block)
+    public void opcode5XY0() {
+
+    }
+
+    //  Explanation: Sets VX to NN.
+    public void opcode6XNN() {
+
+    }
+
+    //  Explanation: Adds NN to VX. (Carry flag is not changed)
+    public void opcode7XNN() {
+
+    }
+
+    //  Explanation: Sets VX to the value of VY.
+    public void opcode8XY0() {
+
+    }
+
+    //  Explanation: Sets VX to VX or VY.
+    //  (Bitwise OR operation)
+    public void opcode8XY1() {
+
+    }
+
+    //  Explanation: Sets VX to VX and VY.
+    //  (Bitwise AND operation)
+    public void opcode8XY2() {
+
+    }
+
+    //  Explanation: Sets VX to VX xor VY.
+    public void opcode8XY3() {
+
+    }
+
+    //  Explanation: Adds VY to VX.
+    //  VF is set to 1 when there's a carry, and to 0 when there isn't.
+    public void opcode8XY4() {
+
+    }
+
+    //  Explanation: VY is subtracted from VX.
+    //  VF is set to 0 when there's a borrow, and 1 when there isn't.
+    public void opcode8XY5() {
+
+    }
+
+    //  Explanation: Stores the least significant bit
+    //  of VX in VFand then shifts VX to the right by 1.
+    public void opcode8XY6() {
+
+    }
+
+    //  Explanation: Sets VX to VY minus VX. VF is set to 0
+    //  when there's a borrow, and 1 when there isn't.
+    public void opcode8XY7() {
+
+    }
+
+    //  Explanation: Stores the most significant bit of VX
+    //  in VF and then shifts VX to the left by 1.
+    public void opcode8XYE() {
+
+    }
+
+    //  Explanation: Stores the most significant bit of VX
+    //  in VF and then shifts VX to the left by 1.
+    public void opcode9XY0() {
+
+    }
+
+    //  Explanation: Sets I to the address NNN.
+    public void opcodeANNN() {
+
+    }
+
+    //  Explanation: Jumps to the address NNN plus V0.
+    public void opcodeBNNN() {
+
+    }
+
+    /**
+     * Sets VX to the result of a bitwise and operation
+     * on a random number (Typically: 0 to 255) and NN.
+     */
+    public void opcodeCXNN() {
+
+    }
+
+    /**
+     * Draws a sprite at coordinate (VX, VY) that has a width
+     * of 8 pixels and a height of N pixels. Each row of 8 pixels
+     * is read as bit-coded starting from memory location I; I value
+     * doesn’t change after the execution of this instruction. As described
+     * above, VF is set to 1 if any screen pixels are flipped from set to unset
+     * when the sprite is drawn, and to 0 if that doesn’t happen
+     */
+    public void opcodeDXYN() {
+
+    }
+
+    /**
+     * Skips the next instruction if the key stored in VX is pressed.
+     * (Usually the next instruction is a jump to skip a code block)
+     */
+    public void opcodeEX9E() {
+
+    }
+
+    /**
+     * Skips the next instruction if the key stored in VX isn't pressed.
+     * (Usually the next instruction is a jump to skip a code block)
+     */
+    public void opcodeEXA1() {
+
+    }
+
+    /**
+     * Sets VX to the value of the delay timer.
+     */
+    public void opcodeFX07() {
+
+    }
+
+    /**
+     * A key press is awaited, and then stored in VX.
+     */
+    public void opcodeFX0A() {
+
+    }
+
+    /**
+     * Sets the delay timer to VX.
+     */
+    public void opcodeFX15() {
+
+    }
+
+    /**
+     * Sets the sound timer to VX.
+     */
+    public void opcodeFX18() {
+
+    }
+
+    /**
+     * Adds VX to I.
+     */
+    public void opcodeFX1E() {
+
+    }
+
+    /**
+     * Sets I to the location of the sprite for the character in VX.
+     * Characters 0-F (in hexadecimal) are represented by a 4x5 font.
+     */
+    public void opcodeFX29() {
+
+    }
+
+    /**
+     * Stores the binary-coded decimal representation of VX, with the most
+     * significant of three digits at the address in I, the middle digit
+     * at I plus 1, and the least significant digit at I plus 2.
+     * (In other words, take the decimal representation of VX, place the
+     * hundreds digit in memory at location in I, the tens digit at
+     * location I+1, and the ones digit at location I+2.)
+     */
+    public void opcodeFX33() {
+
+    }
+
+    /**
+     * Stores V0 to VX (including VX) in memory starting at address I.
+     * The offset from I is increased by 1 for each value written,
+     * but I itself is left unmodified.
+     */
+    public void opcodeFX55() {
+
+    }
+
+    /**
+     * Fills V0 to VX (including VX) with values from memory starting at
+     * address I. The offset from I is increased by 1 for each value written,
+     * but I itself is left unmodified.
+     */
+    public void opcodeFX65() {
+
+    }
 
 }
