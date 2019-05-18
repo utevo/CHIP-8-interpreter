29283943d39795973c8671ae4b016552e02b0d5f
Kowies <kowieski.m98@gmail.com>
2019-05-19 01:10:38 +0200

    Rename class DebugingInfoApp to RegisterInfoApp

--- src/main/java/chip8/app/debug/RegistersInfoApp.java
+++ src/main/java/chip8/app/debug/RegistersInfoApp.java
@@ -1,30 +1,15 @@
-package chip8.app;
+package chip8.app.debug;
 
 import chip8.CPU;
 import chip8.Memory;
-import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
-import lombok.Data;
-
-@Data
-class Register {
-  public String name;
-  public int value;
-
-  Register(String name, int value) {
-      this.name = name;
-      this.value = value;
-  }
-};
 
 public class DebugInfoApp extends Stage {
 
@@ -36,23 +21,22 @@ public class DebugInfoApp extends Stage {
     StackPane layout;
     Scene scene;
 
-    DebugInfoApp(CPU cpu) {
+    public DebugInfoApp(CPU cpu) {
         this.cpu = cpu;
 
         TableColumn<Register, String> nameColumn = new TableColumn<>("Name");
         nameColumn.setMinWidth(200);
         nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
 
-        //Price column
         TableColumn<Register, String> valueColumn = new TableColumn<>("Value");
         valueColumn.setMinWidth(200);
         valueColumn.setCellValueFactory(new PropertyValueFactory<>("value"));
 
 
         table = new TableView<>();
-        table.getColumns().addAll(nameColumn, valueColumn);
 
         refresh();
+        table.getColumns().addAll(nameColumn, valueColumn);
 
         layout = new StackPane();
         layout.getChildren().add(table);
@@ -68,14 +52,32 @@ public class DebugInfoApp extends Stage {
         Memory memory = cpu.getMemory();
 
         ObservableList<Register> registers = FXCollections.observableArrayList();
+        String valueOfRegisterAsHexString;
+        Register newRegister;
 
-        registers.add(new Register("V0", memory.V[0x0]));
-        registers.add(new Register("V1", memory.V[0x1]));
-        registers.add(new Register("V2", memory.V[0x2]));
-        registers.add(new Register("V3", memory.V[0x3]));
-        registers.add(new Register("V4", memory.V[0x4]));
-        registers.add(new Register("V5", memory.V[0x5]));
-        registers.add(new Register("V6", memory.V[0x6]));
+        // PC
+        valueOfRegisterAsHexString = "0x" + Integer.toHexString(memory.PC).toUpperCase();
+        newRegister = new Register("PC", valueOfRegisterAsHexString);
+        registers.add(newRegister);
+
+        // V
+        for (int i = 0; i <= 0xF; ++i) {
+            String iAsHexString = Integer.toHexString(i).toUpperCase();
+            valueOfRegisterAsHexString = "0x" + Integer.toHexString(memory.V[i]).toUpperCase();
+
+            newRegister = new Register("V" + iAsHexString, valueOfRegisterAsHexString);
+            registers.add(newRegister);
+        }
+
+        // I
+        valueOfRegisterAsHexString = "0x" + Integer.toHexString(memory.I).toUpperCase();
+        newRegister = new Register("I", valueOfRegisterAsHexString);
+        registers.add(newRegister);
+
+        // SP
+        valueOfRegisterAsHexString = "0x" + Integer.toHexString(memory.SP).toUpperCase();
+        newRegister = new Register("SP", valueOfRegisterAsHexString);
+        registers.add(newRegister);
 
         table.setItems(registers);
 
